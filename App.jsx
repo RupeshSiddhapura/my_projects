ba98061e3b95ab4b83074f42cb9a3973e09589be
RupeshSiddhapura <siddhapurarupesh@gmail.com>
2024-06-05 13:10:48 +0530

    demo

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Component from './Component/Component'
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    <Component/>
+    </View>
+  )
+}
+
+export default App
---
+++ Component/Component.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Component = () => {
+  return (
+    <View>
+      <Text style= {{fontSize:25,backgroundColor:"yellow",}}>My Name is Rupesh Siddhapura</Text>
+    </View>
+  )
+}
+
+export default Component
